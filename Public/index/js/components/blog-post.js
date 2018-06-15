42274137baf57f06a1872f57b3096d5d440cd3d1
encircles <encircles@163.com>
2018-06-15 17:22:31 +0800

    180615 pm 1

---
+++ Public/index/js/components/blog-post.js
@@ -0,0 +1,9 @@
+Vue.component('blog-post', {
+    props: ['post'],
+    template: `
+    <div class="blog-post">
+      <h3>{{ post.title }}</h3>
+      <div v-html="post.content"></div>
+    </div>
+  `
+});
